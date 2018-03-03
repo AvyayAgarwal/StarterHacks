25401d8cc7aed44a7f97c50001421b485eb7a9fa
SidhaantAnand <sidhaant.anand@gmail.com>
2018-03-03 17:51:58 -0500

    Login v1.2

--- app.js
+++ app.js
@@ -40,10 +40,19 @@
       var password = document.getElementById("pass").value;
       alert(email + " , " + password);
       
-      var signStatus = firebase.auth().signInWithEmailAndPassword(email, password);
-
-      signStatus.catch(err) {
-        console.log("Sign In failed");
+       firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
       }
-    }
   };
